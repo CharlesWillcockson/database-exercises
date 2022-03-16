7269953bcd1b0abac5db79ead92fe3f92d82669f
Charles Willcockson <willcockson.charles@gmail.com>
2022-03-16 10:14:52 -0500

    finished tables exercise

---
+++ albums_migration.sql
@@ -0,0 +1,22 @@
+-- id — auto-incrementing unsigned integer primary key
+-- artist — string for storing the recording artist name
+-- name — string for storing a record name
+-- release_date — integer representing year record was released
+-- sales — floating point value for number of records sold (in millions)
+-- genre — string for storing the record's genre(s)
+
+
+USE codeup_test_db;
+
+DROP TABLE IF EXISTS albums;
+
+CREATE TABLE albums
+(
+    id           INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    artist       VARCHAR(50),
+    name         VARCHAR(100) NOT NULL,
+    release_date INT UNSIGNED,
+    sales        FLOAT,
+    genre        VARCHAR(50),
+    PRIMARY KEY (id)
+);
