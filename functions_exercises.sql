c153ffd8887180ce1674507da456300858f6cd3d
Charles Willcockson <willcockson.charles@gmail.com>
2022-03-17 10:14:33 -0500

    finished functions exercise

--- functions_exercises.sql
+++ functions_exercises.sql
@@ -49,3 +49,12 @@ WHERE year(hire_date) BETWEEN 1990 AND 1999
   AND day(birth_date) = 25
 ORDER BY birth_date ASC,
          hire_date DESC;
+
+# For your query of employees born on Christmas and hired in the 90s, use datediff() to find how many days they have been working at the company (Hint: You might also need to use now() or curdate()).
+
+SELECT *,
+       DATEDIFF(curdate(), hire_date)
+FROM employees
+WHERE year(hire_date) BETWEEN 1990 AND 1999
+  AND month(birth_date) = 12
+  AND day(birth_date) = 25;
