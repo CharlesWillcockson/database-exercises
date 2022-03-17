f5aa60a3a164e3e2166c33f80af20dc791f79dd8
Charles Willcockson <willcockson.charles@gmail.com>
2022-03-17 09:36:44 -0500

    working through functions exercise

---
+++ functions_exercises.sql
@@ -0,0 +1,51 @@
+USE employees;
+
+SELECT first_name, last_name
+FROM employees
+WHERE first_name IN ('Irena', 'Vidya', 'Maya')
+ORDER BY first_name, last_name;
+
+SELECT first_name, last_name
+FROM employees
+WHERE first_name = 'Irena'
+   OR first_name =  'Vidya'
+   OR first_name = 'Maya'
+    AND gender = 'male';
+
+SELECT first_name, last_name, emp_no
+FROM employees
+WHERE last_name LIKE 'E%'
+   OR last_name LIKE '%e'
+ORDER BY emp_no;
+
+SELECT CONCAT(first_name, ' ', last_name)
+FROM employees
+WHERE last_name LIKE 'E%'
+  AND last_name LIKE '%e';
+
+SELECT first_name, last_name
+FROM employees
+WHERE last_name LIKE '%q%'
+  AND last_name NOT LIKE '%qu%';
+
+# All Christmas birthdays
+SELECT first_name, last_name, birth_date
+FROM employees
+WHERE MONTH(birth_date) = 12
+AND DAY(birth_date) = 25;
+
+# All Christmas birthdays hired in the 90s
+SELECT *
+FROM employees
+WHERE year(hire_date) BETWEEN 1990 AND 1999
+  AND month(birth_date) = 12
+  AND day(birth_date) = 25;
+
+# Change the query for employees hired in the 90s and born on Christmas such that the first result is the oldest employee who was hired last. It should be Khun Bernini
+SELECT *
+FROM employees
+WHERE year(hire_date) BETWEEN 1990 AND 1999
+  AND month(birth_date) = 12
+  AND day(birth_date) = 25
+ORDER BY birth_date ASC,
+         hire_date DESC;
