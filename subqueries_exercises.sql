af513f0a8b23377273d285a1885ce87917b2f9d8
Charles Willcockson <willcockson.charles@gmail.com>
2022-03-18 10:49:23 -0500

    finished subqueries no bonuses

--- subqueries_exercises.sql
+++ subqueries_exercises.sql
@@ -5,11 +5,19 @@ FROM employees
 WHERE hire_date IN(
     SELECT hire_date FROM employees
     WHERE emp_no = '101010'
-    )
+    );
 
 SELECT *
 FROM titles
 WHERE emp_no IN(
     SELECT emp_no FROM employees
     WHERE first_name = 'Aamod'
+    );
+
+SELECT first_name, last_name
+FROM employees
+WHERE emp_no IN(
+    SELECT emp_no FROM dept_manager
+    WHERE to_date > CURDATE()
     )
+AND gender = 'F';
